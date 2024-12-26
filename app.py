42b6776de64d2d9c57bf8c836f6bc53cce58d3a4
STEM Mavericks <stemavericks.team@outlook.com>
2024-12-26 14:26:42 +0000

    Auto Commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, url_for, session, request, redirect
 
 app = Flask(__name__)
+app,secret_key =
 
 @app.route('/')
 def home():
