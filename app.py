c32b00704ff6ea0166c6a6bffbf04e97e2bbaad9
STEM Mavericks <stemavericks.team@outlook.com>
2024-12-26 14:48:37 +0000

    Auto Commit

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
+users = {
+    "admin": "password123",
+    "user": "mypassword"
+}
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,23 +19,25 @@ def home():
 @app.route('/dashboard')
 def dashboard():
     if 'username' not in session:
-        flash('Please Log In First!','warning')
+        flash('Please Log In First!', 'warning')
         return redirect(url_for('login'))
     return f"Welcome to your dashboard, {session['username']}!"
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
+        # Authenticate user
         if username in users and users[username] == password:
             session['username'] = username
-            flash('Login Successfull!', 'success')
-            return redirect(url_for('login'))
+            flash('Login Successful!', 'success')
+            return redirect(url_for('dashboard'))
         else:
             flash('Invalid Username or Password', 'danger')
-        return render_template('login.html')
+
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
