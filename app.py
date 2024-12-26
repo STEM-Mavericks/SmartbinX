188a649e964afbb86efda5454b2bc9b71458174a
STEM Mavericks <stemavericks.team@outlook.com>
2024-12-26 14:36:49 +0000

    Auto Commit

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ def home():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
+        if username in users and users[username]
 if __name__ == '__main__':
     app.run(debug=True)
