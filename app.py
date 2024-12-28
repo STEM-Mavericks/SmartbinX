7998276a7c82accdd30e95265722a8679a05616d
STEM Mavericks <stemavericks.team@outlook.com>
2024-12-28 10:38:03 +0000

    Auto Commit

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class User(UserMixin, db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 def validate_username(username):
     return len(username) >= 3 and len(username) <= 20
